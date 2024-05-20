496e7db3df292f80c8fe4f01c5d14cc59a9aeccd
TommasoMardegan <amardegan908@gmail.com>
2024-05-20 21:23:10 +0200

    inizializzato springboot

--- informatica simulazione/webservice/endpoint/src/test/java/com/example/endpoint/EndpointApplicationTests.java
+++ informatica simulazione/webservice/endpoint/src/test/java/com/example/endpoint/EndpointApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.endpoint;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class EndpointApplicationTests {
 
 	@Test
 	void contextLoads() {
